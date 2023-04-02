e4dc6f6a5f84caadddf4ee1cc05b49e6cd88cf2a
Aidan <aidan.mellin@gmail.com>
2023-04-02 19:54:50 -0400

    formatting

--- src/App.js
+++ src/App.js
@@ -23,9 +23,6 @@ function HeroSection() {
 }
 
 function FeaturesSection() {
-  var features = {
-    "learning-modules": ["Learning Modules", "Choose from a variety of specific learning modules.", ""]
-  }
   return (
     <section id="features">
       <AddFeature
